aad319b104d73defcca9cedf0779561227c508b6
Paul-AH01 <132997781+Paul-AH01@users.noreply.github.com>
2023-06-20 15:12:25 +0100

    Update router.js

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const books = require("./booksController.js")
 
 // getting all of the todos
+router.get("/")
 router.get("/books", books.index)
 router.post("/books/create", books.create)
 router.get("/books/:id", books.getSpecificBooks)
