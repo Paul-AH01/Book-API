7713b7248787b762b3dbd34a9864f0a31ef48d32
Paul-AH01 <132997781+Paul-AH01@users.noreply.github.com>
2023-06-20 15:57:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const port = process.env.PORT||3003
 const router = require("./router")
+const mongoose = require("mongoose")
+
+try {
+    mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    console.log("connected to DB")
+} catch (error) {
+    console.log(error)
+}
 
 app.use(express.json())
 
